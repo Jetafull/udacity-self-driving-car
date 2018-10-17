972b3a6cefd382bbcba09507bc9653093ee52144
jiangtao <jetafull@gmail.com>
2018-10-18 00:49:33 +0900

    Reformat code using Xcode

--- carnd-p5-extended-kalman-filter/src/kalman_filter.cpp
+++ carnd-p5-extended-kalman-filter/src/kalman_filter.cpp
@@ -13,12 +13,12 @@ KalmanFilter::~KalmanFilter() {}
 
 void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
                         MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
-  x_ = x_in;
-  P_ = P_in;
-  F_ = F_in;
-  H_ = H_in;
-  R_ = R_in;
-  Q_ = Q_in;
+    x_ = x_in;
+    P_ = P_in;
+    F_ = F_in;
+    H_ = H_in;
+    R_ = R_in;
+    Q_ = Q_in;
 }
 
 void KalmanFilter::Predict() {
@@ -35,7 +35,7 @@ void KalmanFilter::Update(const VectorXd &z) {
     MatrixXd Si = S.inverse();
     MatrixXd PHt = P_*Ht;
     MatrixXd K = PHt*Si;
-
+    
     // new estimate
     x_ = x_ + K*y;
     long x_size = x_.size();
@@ -47,18 +47,18 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
     // create Jacobian of H
     Tools tools;
     MatrixXd Hj = tools.CalculateJacobian(x_); 
-
+    
     // recover state parameters
     float px = x_(0);
     float py = x_(1);
     float vx = x_(2);
     float vy = x_(3);
-
+    
     // map states to measurements direct using h function
     float rho = sqrt(px*px+py*py);
     VectorXd z_pred(3);
     z_pred << rho, atan2(py, px), (px*vx+py*vy)/rho;
-
+    
     // calculate y
     // normalize phi to between -pi and pi
     // assert(-M_PI < y(1) & y(1) < M_PI);
