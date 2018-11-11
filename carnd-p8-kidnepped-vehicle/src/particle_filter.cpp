a84a0d0b2f0b7d1303557367af685d2c0609e318
jiangtao <jetafull@gmail.com>
2018-11-11 20:45:10 +0900

    Add gaussian random noise in init

--- carnd-p8-kidnepped-vehicle/src/particle_filter.cpp
+++ carnd-p8-kidnepped-vehicle/src/particle_filter.cpp
@@ -25,19 +25,28 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
   // Add random Gaussian noise to each particle.
   // NOTE: Consult particle_filter.h for more information about this method (and
   // others in this file).
+  if (!is_initialized) {
+    default_random_engine gen;
 
-  // Initialize particles with random generators
-  for (int i = 0; i < num_particles; i++) {
-    Particle particle = {};
-    particle.x = x;
-    particle.y = y;
-    particle.theta = theta;
-    particle.weight = 1.0;
+    // Initialize particles with random generators
+    for (int i = 0; i < num_particles; i++) {
+      Particle particle = {};
 
-    particles.push_back(particle);
+      particles.push_back(particle);
+
+      // Add random Gaussian noise
+      normal_distribution<double> dist_x(x, std[0]);
+      normal_distribution<double> dist_y(y, std[1]);
+      normal_distribution<double> dist_theta(theta, std[2]);
+
+      particles[i].x = dist_x(gen);
+      particles[i].y = dist_y(gen);
+      particles[i].theta = dist_theta(gen);
+      particle.weight = 1.0;
+    }
+
+    is_initialized = true;
   }
-
-  is_initialized = true;
 }
 
 void ParticleFilter::prediction(double delta_t, double std_pos[],
@@ -48,11 +57,7 @@ void ParticleFilter::prediction(double delta_t, double std_pos[],
   //  http://en.cppreference.com/w/cpp/numeric/random/normal_distribution
   //  http://www.cplusplus.com/reference/random/default_random_engine/
 
-  // Setup Gaussian random generators parameters
   default_random_engine gen;
-  double std_x = std_pos[0];
-  double std_y = std_pos[1];
-  double std_theta = std_pos[2];
 
   for (int i = 0; i < num_particles; i++) {
     double x = particles[i].x;
@@ -70,9 +75,9 @@ void ParticleFilter::prediction(double delta_t, double std_pos[],
     }
 
     // Add random Gaussian noise
-    normal_distribution<double> dist_x(x, std_x);
-    normal_distribution<double> dist_y(y, std_y);
-    normal_distribution<double> dist_theta(theta, std_theta);
+    normal_distribution<double> dist_x(x, std_pos[0]);
+    normal_distribution<double> dist_y(y, std_pos[1]);
+    normal_distribution<double> dist_theta(theta, std_pos[2]);
 
     particles[i].x = dist_x(gen);
     particles[i].y = dist_y(gen);
@@ -83,30 +88,26 @@ void ParticleFilter::prediction(double delta_t, double std_pos[],
 void ParticleFilter::dataAssociation(std::vector<LandmarkObs> predicted,
                                      std::vector<LandmarkObs>& observations) {
   // TODO: Find the predicted measurement that is closest to each observed
-  // measurement and assign the
-  //   observed measurement to this particular landmark.
-  // NOTE: this method will NOT be called by the grading code. But you will
-  // probably find it useful to
-  //   implement this method and use it as a helper during the updateWeights
-  //   phase.
+  // measurement and assign the observed measurement to this particular
+  // landmark. NOTE: this method will NOT be called by the grading code. But you
+  // will probably find it useful to implement this method and use it as a
+  // helper during the updateWeights phase.
 }
 
 void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
                                    const std::vector<LandmarkObs>& observations,
                                    const Map& map_landmarks) {
   // TODO: Update the weights of each particle using a mult-variate Gaussian
-  // distribution. You can read
-  //   more about this distribution here:
+  // distribution. You can read more about this distribution here:
   //   https://en.wikipedia.org/wiki/Multivariate_normal_distribution
   // NOTE: The observations are given in the VEHICLE'S coordinate system. Your
-  // particles are located
-  //   according to the MAP'S coordinate system. You will need to transform
-  //   between the two systems. Keep in mind that this transformation requires
-  //   both rotation AND translation (but no scaling). The following is a good
-  //   resource for the theory:
-  //   https://www.willamette.edu/~gorr/classes/GeneralGraphics/Transforms/transforms2d.htm
-  //   and the following is a good resource for the actual equation to implement
-  //   (look at equation 3.33 http://planning.cs.uiuc.edu/node99.html
+  // particles are located according to the MAP'S coordinate system. You will
+  // need to transform between the two systems. Keep in mind that this
+  // transformation requires both rotation AND translation (but no scaling). The
+  // following is a good resource for the theory:
+  // https://www.willamette.edu/~gorr/classes/GeneralGraphics/Transforms/transforms2d.htm
+  // and the following is a good resource for the actual equation to implement
+  // (look at equation 3.33 http://planning.cs.uiuc.edu/node99.html
 }
 
 void ParticleFilter::resample() {
