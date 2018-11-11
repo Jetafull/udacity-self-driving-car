a577a504ce5c8b1c87f94abb292f41624c60210e
jiangtao <jetafull@gmail.com>
2018-11-11 18:01:06 +0900

    Init

---
+++ carnd-p8-kidnepped-vehicle/src/particle_filter.cpp
@@ -0,0 +1,156 @@
+/*
+ * particle_filter.cpp
+ *
+ *  Created on: Dec 12, 2016
+ *      Author: Tiffany Huang
+ */
+
+#include <math.h>
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <numeric>
+#include <random>
+#include <sstream>
+#include <string>
+
+#include "particle_filter.h"
+
+using namespace std;
+
+void ParticleFilter::init(double x, double y, double theta, double std[]) {
+  // TODO: Set the number of particles. Initialize all particles to first
+  // position (based on estimates of
+  //   x, y, theta and their uncertainties from GPS) and all weights to 1.
+  // Add random Gaussian noise to each particle.
+  // NOTE: Consult particle_filter.h for more information about this method (and
+  // others in this file).
+
+  // Initialize particles with random generators
+  for (int i = 0; i < num_particles; i++) {
+    Particle particle = {};
+    particle.x = x;
+    particle.y = y;
+    particle.theta = theta;
+    particle.weight = 1.0;
+
+    particles.push_back(particle);
+  }
+
+  is_initialized = true;
+}
+
+void ParticleFilter::prediction(double delta_t, double std_pos[],
+                                double velocity, double yaw_rate) {
+  // TODO: Add measurements to each particle and add random Gaussian noise.
+  // NOTE: When adding noise you may find std::normal_distribution and
+  // std::default_random_engine useful.
+  //  http://en.cppreference.com/w/cpp/numeric/random/normal_distribution
+  //  http://www.cplusplus.com/reference/random/default_random_engine/
+
+  // Setup Gaussian random generators parameters
+  default_random_engine gen;
+  double std_x = std_pos[0];
+  double std_y = std_pos[1];
+  double std_theta = std_pos[2];
+
+  for (int i = 0; i < num_particles; i++) {
+    double x = particles[i].x;
+    double y = particles[i].y;
+    double theta = particles[i].theta;
+
+    // Apply motion model
+    if (yaw_rate == 0) {
+      x += velocity * delta_t * cos(theta);
+      y += velocity * delta_t * sin(theta);
+    } else {
+      x += velocity / yaw_rate * (sin(theta + yaw_rate * delta_t) - sin(theta));
+      y += velocity / yaw_rate * (cos(theta) - cos(theta + yaw_rate * delta_t));
+      theta += yaw_rate * delta_t;
+    }
+
+    // Add random Gaussian noise
+    normal_distribution<double> dist_x(x, std_x);
+    normal_distribution<double> dist_y(y, std_y);
+    normal_distribution<double> dist_theta(theta, std_theta);
+
+    particles[i].x = dist_x(gen);
+    particles[i].y = dist_y(gen);
+    particles[i].theta = dist_theta(gen);
+  }
+}
+
+void ParticleFilter::dataAssociation(std::vector<LandmarkObs> predicted,
+                                     std::vector<LandmarkObs>& observations) {
+  // TODO: Find the predicted measurement that is closest to each observed
+  // measurement and assign the
+  //   observed measurement to this particular landmark.
+  // NOTE: this method will NOT be called by the grading code. But you will
+  // probably find it useful to
+  //   implement this method and use it as a helper during the updateWeights
+  //   phase.
+}
+
+void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
+                                   const std::vector<LandmarkObs>& observations,
+                                   const Map& map_landmarks) {
+  // TODO: Update the weights of each particle using a mult-variate Gaussian
+  // distribution. You can read
+  //   more about this distribution here:
+  //   https://en.wikipedia.org/wiki/Multivariate_normal_distribution
+  // NOTE: The observations are given in the VEHICLE'S coordinate system. Your
+  // particles are located
+  //   according to the MAP'S coordinate system. You will need to transform
+  //   between the two systems. Keep in mind that this transformation requires
+  //   both rotation AND translation (but no scaling). The following is a good
+  //   resource for the theory:
+  //   https://www.willamette.edu/~gorr/classes/GeneralGraphics/Transforms/transforms2d.htm
+  //   and the following is a good resource for the actual equation to implement
+  //   (look at equation 3.33 http://planning.cs.uiuc.edu/node99.html
+}
+
+void ParticleFilter::resample() {
+  // TODO: Resample particles with replacement with probability proportional to
+  // their weight. NOTE: You may find std::discrete_distribution helpful here.
+  //   http://en.cppreference.com/w/cpp/numeric/random/discrete_distribution
+}
+
+Particle ParticleFilter::SetAssociations(Particle& particle,
+                                         const std::vector<int>& associations,
+                                         const std::vector<double>& sense_x,
+                                         const std::vector<double>& sense_y) {
+  // particle: the particle to assign each listed association, and association's
+  // (x,y) world coordinates mapping to
+  // associations: The landmark id that goes along with each listed association
+  // sense_x: the associations x mapping already converted to world coordinates
+  // sense_y: the associations y mapping already converted to world coordinates
+
+  particle.associations = associations;
+  particle.sense_x = sense_x;
+  particle.sense_y = sense_y;
+}
+
+string ParticleFilter::getAssociations(Particle best) {
+  vector<int> v = best.associations;
+  stringstream ss;
+  copy(v.begin(), v.end(), ostream_iterator<int>(ss, " "));
+  string s = ss.str();
+  s = s.substr(0, s.length() - 1);  // get rid of the trailing space
+  return s;
+}
+string ParticleFilter::getSenseX(Particle best) {
+  vector<double> v = best.sense_x;
+  stringstream ss;
+  copy(v.begin(), v.end(), ostream_iterator<float>(ss, " "));
+  string s = ss.str();
+  s = s.substr(0, s.length() - 1);  // get rid of the trailing space
+  return s;
+}
+string ParticleFilter::getSenseY(Particle best) {
+  vector<double> v = best.sense_y;
+  stringstream ss;
+  copy(v.begin(), v.end(), ostream_iterator<float>(ss, " "));
+  string s = ss.str();
+  s = s.substr(0, s.length() - 1);  // get rid of the trailing space
+  return s;
+}
